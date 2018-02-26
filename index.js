eb534848cf585dc1b705c97d80614fee5138aedf
George Valdes <gv2186@gmail.com>
2018-02-26 17:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function happyHolidayTo(holiday, name)
 
 function holidayCountdown(holiday, days)
 {
-  return "It\'s ${days} days until ${holiday}!"
+  return 'It\'s ${days} days until ${holiday}!'
 }
 
-holidayCountdown("Mother's Day", 20)
 
